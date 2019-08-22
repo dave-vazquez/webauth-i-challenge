48f62bf7c2aa73fc1d637ea75a8e0edc912cf6d1
Dave Vazquez <dmvaz.li@gmail.com>
2019-08-21 22:07:49 -0400

    implemented GET '/users' enpoint, fixed authorize middleware

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -42,25 +42,37 @@ router.post('/login', async (req, res, next) => {
   }
 });
 
+router.get('/users', authorize, async (req, res, next) => {
+  try {
+    const users = await Users.find();
+    res.status(200).json(users);
+  } catch (err) {
+    next(err);
+  }
+});
+
 /********************************************************
  *                  CUSTOM MIDDLEWARE                   *
  ********************************************************/
-function authorize(req, res, next) {
+async function authorize(req, res, next) {
   const { username, password } = req.headers;
 
-  try {
-    const user = await Users.findBy({ username });
+  if (username && password) {
+    try {
+      const user = await Users.findBy({ username });
 
-    user && bcrypt.compareSync(password, user.password)
-      ? res.status(200).json({
-          next();
-        })
-      : res.status(403).json({
-          message: 'Forbidden'
-        });
-
-  } catch(err) {
-    next(err);
+      user && bcrypt.compareSync(password, user.password)
+        ? next()
+        : res.status(403).json({
+            message: 'Forbidden - Invalid Credentials'
+          });
+    } catch (err) {
+      next(err);
+    }
+  } else {
+    res.status(403).json({
+      message: 'Forbidden - Invalid Credentials'
+    });
   }
 }
 
