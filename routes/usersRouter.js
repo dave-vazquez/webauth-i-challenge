aec9f3e1bfc5bbd67e8cfdf2dff206381ef144ae
Dave Vazquez <dmvaz.li@gmail.com>
2019-08-21 21:27:20 -0400

    POST '/api/register endpoint implemented

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
-const Users = require('../data/db-config');
+const Users = require('./usersModel');
 
 const router = express.Router();
+
+/********************************************************
+ *                   POST /api/register                 *
+ ********************************************************/
+router.post('/register', async (req, res, next) => {
+  const user = req.body;
+
+  try {
+    user.password = bcrypt.hashSync(user.password, 10);
+
+    const newUser = await Users.add(user);
+
+    res.status(201).json(newUser);
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
