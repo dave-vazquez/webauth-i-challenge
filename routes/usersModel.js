aa8a0ca8c1ce80d6a6668b5c138b7d16c276f729
Dave Vazquez <dmvaz.li@gmail.com>
2019-08-21 21:19:15 -0400

    add and findById functions implemented

--- routes/usersModel.js
+++ routes/usersModel.js
@@ -4,6 +4,17 @@ module.exports = {
   add
 };
 
+function findById(id) {
+  return db('users')
+    .where({ id })
+    .first();
+}
+
 function add(user) {
-  return;
+  return db('users')
+    .insert(user, 'id') // second arg tells knex to return an array of all rows of the specified column
+    .then(ids => {
+      const [id] = ids; // destructures first element of the returned array
+      return findById(id);
+    });
 }
