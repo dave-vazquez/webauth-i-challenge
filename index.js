060e4411bdb1a3560f0130bfcf44daf58dae133a
Dave Vazquez <dmvaz.li@gmail.com>
2019-08-21 22:43:00 -0400

    added endpoint for root of api

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ const port = process.env.PORT || 000;
 server.listen(port, () => {
   console.log(`*** Listening on port ${port}***`);
 });
+
+server.get('/', (req, res) => {
+  res.send('<h1>Users API</h1>');
+});
