643cf4f2e8121671a9a82489cad120c0b87531cd
Dave Vazquez <dmvaz.li@gmail.com>
2019-08-24 15:48:23 -0400

    updated import path, commented out custom middleware

---
+++ routes/users/usersRouter.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const Users = require('./usersModel');
+
+const router = express.Router();
+
+/********************************************************
+ *                   POST /api/register                 *
+ ********************************************************/
+router.post('/register', async (req, res, next) => {
+  const user = req.body;
+
+  try {
+    user.password = bcrypt.hashSync(user.password, 10);
+
+    const newUser = await Users.add(user);
+
+    res.status(201).json(newUser);
+  } catch (err) {
+    next(err);
+  }
+});
+
+/********************************************************
+ *                   POST /api/login                    *
+ ********************************************************/
+router.post('/login', async (req, res, next) => {
+  const { username, passwordGuess } = req.body;
+
+  try {
+    const user = await Users.findBy({ username });
+
+    user && bcrypt.compareSync(passwordGuess, user.password)
+      ? res.status(200).json({
+          message: `ayyy ${username}`
+        })
+      : res.status(400).json({
+          message: 'Invalid Credentials'
+        });
+  } catch (err) {
+    next(err);
+  }
+});
+
+/********************************************************
+ *                  CUSTOM MIDDLEWARE                   *
+ ********************************************************/
+// async function authorize(req, res, next) {
+//   const { username, password } = req.headers;
+
+//   if (username && password) {
+//     try {
+//       const user = await Users.findBy({ username });
+
+//       user && bcrypt.compareSync(password, user.password)
+//         ? next()
+//         : res.status(403).json({
+//             message: 'Forbidden - Invalid Credentials'
+//           });
+//     } catch (err) {
+//       next(err);
+//     }
+//   } else {
+//     res.status(403).json({
+//       message: 'Forbidden - Invalid Credentials'
+//     });
+//   }
+// }
+
+module.exports = router;
